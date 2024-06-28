5fffabcf274694935849548649afb63433f67efc
Here <programaciondairo@gmail.com>
2024-06-28 23:53:40 +0200

    Add progress

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,27 @@
-import {Elements,CardElement} from '@stripe/react-stripe-js';
+import {Elements,CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
+
+import "bootswatch/dist/lux/bootstrap.min.css"
 
 const stripePromise = loadStripe('pk_test_51NFGMvC5UXN5nuNS1bUdcmpKFVvvfVsI4cO4kZ5CUJYbreOGg3pgJCQZCxNK4bRbd69avZiIL6FCqBD0pPUNpRHz00owdxuhjw');
 const CheckoutForm = ()=>{
-  return <form>
+
+  const stripe = useStripe();
+  const elements = useElements();
+const handleSubmit = async (e) =>{
+  e.preventDefaul();
+  const {error, paymentMethod} = await stripe.createPaymentMethod({
+  type: 'card',
+  card: elements.getElement(CardElement),
+});
+
+}
+
+  return <form onSubmit={handleSubmit}>
     <CardElement/>
+    <button>
+      Buy
+    </button>
   </form>
 }
 
